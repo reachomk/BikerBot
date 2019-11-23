a3ab51c85a79ffbbddd9cd077e7f28d6b3bde4d0
Your Name <you@example.com>
2019-11-23 15:33:33 -0800

    Fix stuff

--- main.js
+++ main.js
@@ -35,7 +35,13 @@ commands = function(message, bot) {
 			message.channel.send(deep_thoughts[Math.floor(Math.random() * deep_thoughts.length)]);
 		}
 		if (message.content.includes("history_meme")) { //Stolen from https://github.com/sodiumkid/Dr-Ferrel/blob/13f2bc9329983e579e1ca8b72cd7b5ad5fd0bb37/functions.js#L29
-			var random = (Math.floor(Math.random() * Math.floor(527))) + 1
+			history_meme(bot)
+		}
+	}
+}
+
+history_meme = function(bot) {
+	var random = (Math.floor(Math.random() * Math.floor(527))) + 1
  			var number = "";
  			if (random < 10) {
    				number = "00" + random;
@@ -50,12 +56,9 @@ commands = function(message, bot) {
  				var imageName = "https://res.cloudinary.com/drferrel/image/upload/v1568689715/memes/meme" + random + ".jpg";
  			}
  			catch(exception) {
- 				let rand = (Math.floor(Math.random() * Math.floor(527))) + 1;
- 				var imageName = "https://res.cloudinary.com/drferrel/image/upload/v1568689715/memes/meme" + random + ".jpg";
+ 				history_meme();
  			}
  			message.channel.send({
      		file: imageName
  			});
-		}
-	}
 }
