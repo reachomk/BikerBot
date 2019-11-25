dea5aa0f602a71a088eb3755b966e12b28d2193e
Your Name <you@example.com>
2019-11-25 10:13:22 -0800

    Add help command.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ require ('dotenv').config();
 
 bot.on("ready", () => {
   console.log("I am alive!");
-  client.user.setPresence({ game: { name: "Use .help to see commands. " }, status: 'idle' });
+  bot.user.setPresence({ game: { name: "Use .help to see commands. " }, status: 'idle' });
 });
 
 bot.on("message", (message) => {
