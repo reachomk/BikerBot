fd4fdbbfa040b5540b3460f18f3229de454017f9
Your Name <you@example.com>
2019-11-29 21:20:48 -0800

    music

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 var functions = require('./functions.js');
-
+bot.music = require("discord.js-musicbot-addon");
 require ('dotenv').config();
 
 bot.on("ready", () => {
   console.log("I am alive!");
   bot.user.setPresence({ game: { name: "Use .help to see commands. " }});
+});
+
+bot.music.start(bot, {
+  youtubeKey: process.env.YOUTUBE_KEY,
+  botPrefix: '.' || '!',
 });
 
 bot.on("message", (message) => {
